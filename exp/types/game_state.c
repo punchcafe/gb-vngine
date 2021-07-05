71478bb2eba88733365aa42d60370a968e79be1a
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-07-06 00:21:23 +0100

    :sparkles: add game state format

--- exp/types/game_state.c
+++ exp/types/game_state.c
@@ -1,6 +1,24 @@
 #ifndef GAMESTATE_TYPE_DEFINITION
 #define GAMESTATE_TYPE_DEFINITION
 struct GameState {
-    int some_var;
+
+    // *** INTEGER VARIABLES *** 
+
+    int some_int_1;
+    int some_int_2;
+    int some_int_3;
+
+
+    // *** BOOLEAN VARIABLES ***
+
+    short some_bool_var;
+    short some_other_bool_var;
+
+
+    // STRING VARIABLES
+
+    char some_string_one [50]; // TODO: make a max value, or instead calculate max possible value?
+    char some_string_two [50];
+
 };
 #endif
