9fb7e985b8ec304683f6241faea3c085b1bcd86a
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-07-27 23:31:21 +0100

    :recycle: refactor gsm

--- exp/gen/game_state_mutations.c
+++ exp/gen/game_state_mutations.c
@@ -1,6 +1,4 @@
 #include "../types/game_state.c"
 
-void do_nothing(struct GameState * game_state)
-{
-    
-}
+void do_nothing(struct GameState * game_state){}
+void(*do_nothing_array[1])(struct GameState *) = {&do_nothing};
