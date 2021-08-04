947d0c57aab49d99339d58bfbc10ab0c6cf07b0e
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-08-04 21:58:52 +0100

    :sparkles: add full sprite rendering

--- exp/narrative_modes/dialogue_mode.c
+++ exp/narrative_modes/dialogue_mode.c
@@ -439,36 +439,70 @@ unsigned char TileLabel[] =
   0x02,0x01,0x27,0xC3,0xB1,0xE5,0xE3,0xF7
 };
 
+#define FOCUS_MODE_X_OFFSET 45
+#define FOCUS_MODE_Y_OFFSET 40
+
+void set_focus_tile(unsigned char * patterns)
+{
+  set_sprite_data(0, (2 * 2)*80, patterns);
+  for(int i = 0; i < 40; i++)
+  {
+    set_sprite_tile(i, i);
+    move_sprite(i,(i%10)*8 + FOCUS_MODE_X_OFFSET, (i/10)*16 + FOCUS_MODE_Y_OFFSET);
+  }
+}
 
 void set_character_tile(unsigned short left_offset, unsigned char * patterns)
 {
     set_sprite_data(0, (2 * 2)*80, patterns);
-    for(unsigned short i = 0; i < 40; i++)
+    for(unsigned short i_0 = 0; i_0 < 5; i_0++)
     {
-        if(i == 0 || i == 6)
-        {
-            continue;
-        }
+      // render first row missing top left and right dual tiles
+      unsigned short x_0 = (i_0*8) + 8 + 8 + left_offset;
+      unsigned short y_0 = 16 + 8;
+      set_sprite_tile(i_0, i_0);
+      move_sprite(i_0, x_0, y_0);
+
+    }
+    for(unsigned short i = 7; i < 43; i++)
+    {
+        // accomodate for missing two tiles in first row, also adjusts sprite num
+        unsigned int sprite_number = i - 2;
         unsigned short x = (i%7) * 8 + 8 + left_offset;
         unsigned short y = (i/7) * 16 + 16 + 8 * 1;
-        set_sprite_tile(i, i);
-        move_sprite(i, x, y);
+        set_sprite_tile(sprite_number, i);
+        move_sprite(sprite_number, x, y);
     }
+}
+
+void set_character_tile_left(unsigned char * patterns) 
+{
+  set_character_tile(16, patterns);
+}
+
+void set_character_tile_right(unsigned char * patterns) 
+{
+  set_character_tile(88, patterns);
 }
 
 int main(){
     SPRITES_8x16;
     while(1)
     {
-        set_character_tile(16, TileLabel);
+        set_focus_tile(TileLabel);
+        delay(100);
         SHOW_SPRITES;
         delay(1000);
         HIDE_SPRITES;
+        set_character_tile_left(TileLabel);
         delay(100);
-        set_character_tile(80, RHSLabel);
         SHOW_SPRITES;
         delay(1000);
         HIDE_SPRITES;
+        set_character_tile_right(RHSLabel);
         delay(100);
+        SHOW_SPRITES;
+        delay(1000);
+        HIDE_SPRITES;
     }
 }
