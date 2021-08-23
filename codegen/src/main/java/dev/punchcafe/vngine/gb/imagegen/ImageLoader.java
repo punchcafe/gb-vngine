00467b1de6f70e7a027ca87636e22811cdb48214
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-08-23 23:12:31 +0100

    :construction: image converter

---
+++ codegen/src/main/java/dev/punchcafe/vngine/gb/imagegen/ImageLoader.java
@@ -0,0 +1,148 @@
+package dev.punchcafe.vngine.gb.imagegen;
+
+import lombok.NonNull;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class ImageLoader {
+
+    //TODO: make dynamic
+    private static Map<String, PixelValue> HEX_CONVERTER = Map.of("ffffff", PixelValue.VAL_0,
+            "c0c0c0", PixelValue.VAL_2,
+            "808080", PixelValue.VAL_1,
+            "000000", PixelValue.VAL_3);
+
+    public enum PixelValue {
+        VAL_0,VAL_1,VAL_2,VAL_3;
+    }
+
+    public static class Tile {
+
+        private static char ZERO_HEX = 0x0;
+        private static char ONE_HEX = 0x1;
+        private PixelValue[][] internalArray = new PixelValue[8][8];
+
+        //TODO: have builder which blows up if null
+        public void setPixel(@NonNull final PixelValue value, int x, int y){
+            internalArray[y][x] = value;
+        }
+
+        private String toGBDKCode(){
+            return Arrays.stream(this.internalArray)
+                    .map(this::getRowCharPair)
+                    .flatMapToInt(this::extractCharacterAsInt)
+                    .mapToObj(this::formatHex)
+                    .map(this::prependHexDelimiter)
+                    .collect(Collectors.joining(","));
+        }
+
+        private IntStream extractCharacterAsInt(final int[] pair){
+            return IntStream.of(pair[0], pair[1]);
+        }
+
+        private String formatHex(final int c){
+            return  String.format("%02x", c);
+        }
+
+        private String prependHexDelimiter(final String hex){
+            return  "0x".concat(hex);
+        }
+
+        private int[] getRowCharPair(final PixelValue[] values){
+            int[] blankPair = {0x0, 0x0};
+            for(int i = 0; i<8; i++){
+                final int bitshift = 7 - i;
+                final int[] pair = getPixelBitPair(values[i]);
+                pair[0] = pair[0] << bitshift;
+                pair[1] = pair[1] << bitshift;
+                blankPair[0] = blankPair[0] | pair[0];
+                blankPair[1] = blankPair[1] | pair[1];
+
+            }
+            return blankPair;
+        }
+
+        private int[] getPixelBitPair(final PixelValue value){
+            switch (value){
+                case VAL_0:
+                    return new int[]{ZERO_HEX, ZERO_HEX};
+                case VAL_1:
+                    return new int[]{ZERO_HEX, ONE_HEX};
+                case VAL_2:
+                    return new int[]{ONE_HEX, ZERO_HEX};
+                case VAL_3:
+                    return new int[]{ONE_HEX, ONE_HEX};
+                default:
+                    throw new UnsupportedOperationException();
+            }
+        }
+
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        File input = new File("src/main/resources/image-res/sample-png.png");
+        BufferedImage image = ImageIO.read(input);
+        final var rgb = image.getRGB(0,0);
+        String [][] rgbs = new String[image.getWidth()][image.getHeight()];
+        Tile[] tiles = new Tile[80];
+        for(int i = 0; i < 80; i++){
+            tiles[i] = new Tile();
+        }
+        if(image.getWidth() != 56 || image.getHeight() != 96){
+            throw new IllegalArgumentException();
+        }
+
+        for(int i = 8; i < image.getWidth() - 8; i++){
+            // first rows
+            for(int j = 0; j < 16; j++) {
+                final var tileIndex = (j / 8)*5  + ((i - 8) / 8);
+                final var thisTile = tiles[tileIndex];
+
+                rgbs[i][j] = Integer.toHexString(image.getRGB(i, j)).substring(2,8);
+                final var tileValue = getPixelValue(Integer.toHexString(image.getRGB(i, j)).substring(2,8));
+                thisTile.setPixel(tileValue, i % 8, j%8);
+            }
+        }
+        for(int i = 0; i < image.getWidth(); i++){
+            for(int j = 16; j < image.getHeight(); j++){
+                final var tileIndex = (((j / 8) * 7) + (i / 8)) - 4; //tile offset
+                final var thisTile = tiles[tileIndex];
+
+                rgbs[i][j] = Integer.toHexString(image.getRGB(i, j)).substring(2,8);
+                final var tileValue = getPixelValue(Integer.toHexString(image.getRGB(i, j)).substring(2,8));
+                thisTile.setPixel(tileValue, i % 8, j%8);
+            }
+        }
+
+        final var code = Arrays.stream(tiles).map(Tile::toGBDKCode)
+                .collect(Collectors.joining(",", "{", "}"));
+
+        final var distinctCodes = Arrays.stream(rgbs)
+                .flatMap(Arrays::stream)
+                .distinct()
+                .map(ImageLoader::getPixelValue)
+                .collect(Collectors.toList());
+
+        System.out.println("hoi");
+    }
+
+    private static PixelValue getPixelValue(final String strValue){
+        if(HEX_CONVERTER.get(strValue) == null){
+            System.out.println("hello evile");
+        }
+        return Optional.of(strValue)
+                .map(HEX_CONVERTER::get)
+                .orElseThrow(IllegalStateException::new);
+    }
+}
---
+++ codegen/src/test/java/dev/punchcafe/vngine/gb/imagegen/ImageLoaderTest.java
@@ -0,0 +1,13 @@
+package dev.punchcafe.vngine.gb.imagegen;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class ImageLoaderTest {
+
+    @Test
+    void rendersImage() throws IOException {
+        ImageLoader.main(null);
+    }
+}
