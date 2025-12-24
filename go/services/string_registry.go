30a7bb87929d473c86158b567ef53aa6453c4ae8
Luca Fruzza <lfruzza@hotmail.co.uk>
2025-12-24 02:42:50 +0000

    feat(go): add services.BuildStringRegistry

--- go/services/string_registry.go
+++ go/services/string_registry.go
@@ -1,24 +1,26 @@
 package services
 
-import "fmt"
+import (
+	"fmt"
+
+	p "punchcafe.dev/gb-vngine/services/predicate"
+	"punchcafe.dev/gb-vngine/services/predicate/registry"
+)
 
 type StringRegistry struct {
 	// treat 0 uint as nil
 	registry map[string]uint
 }
 
-func NewRegistry() StringRegistry {
-	return StringRegistry{make(map[string]uint)}
-}
+func BuildStringRegistry(ps *registry.Registry) *StringRegistry {
+	sr := NewRegistry()
+	v := expressionStringExtractor{sr: &sr}
 
-// Idempotent
-func (sr *StringRegistry) AddString(value string) {
-	lookup := sr.registry[value]
-	if lookup != 0 {
-		// already added
-		return
+	for e := range ps.AllRegisteredPredicates() {
+		e.AcceptVisitor(&v)
 	}
-	sr.registry[value] = uint(len(sr.registry) + 1)
+
+	return &sr
 }
 
 func (sr *StringRegistry) Reference(value string) (string, error) {
@@ -44,4 +46,61 @@ func (st *StringRegistry) AllStrings() []StringEntry {
 
 func constantName(index uint) string {
 	return fmt.Sprintf("STRING_REG_%d", index)
+}
+
+func NewRegistry() StringRegistry {
+	return StringRegistry{make(map[string]uint)}
+}
+
+// Idempotent
+func (sr *StringRegistry) AddString(value string) {
+	lookup := sr.registry[value]
+	if lookup != 0 {
+		// already added
+		return
+	}
+	sr.registry[value] = uint(len(sr.registry) + 1)
+}
+
+// Expression traversal
+
+type expressionStringExtractor struct {
+	sr *StringRegistry
+}
+
+func (ese *expressionStringExtractor) VisitVariableReference(_ p.VariableReference) {}
+func (ese *expressionStringExtractor) VisitNumberLiteral(_ p.NumberLiteral)         {}
+func (ese *expressionStringExtractor) VisitBoolLiteral(_ p.BoolLiteral)             {}
+
+func (ese *expressionStringExtractor) VisitStringLiteral(e p.StringLiteral) {
+	ese.sr.AddString(string(e))
+}
+
+func (ese *expressionStringExtractor) VisitBrackets(e p.Brackets) {
+	e.InnerExpression.AcceptVisitor(ese)
+}
+
+func (ese *expressionStringExtractor) VisitMoreThan(e p.MoreThan) {
+	e.Lhs.AcceptVisitor(ese)
+	e.Rhs.AcceptVisitor(ese)
+}
+
+func (ese *expressionStringExtractor) VisitLessThan(e p.LessThan) {
+	e.Lhs.AcceptVisitor(ese)
+	e.Rhs.AcceptVisitor(ese)
+}
+
+func (ese *expressionStringExtractor) VisitAnd(e p.And) {
+	e.Lhs.AcceptVisitor(ese)
+	e.Rhs.AcceptVisitor(ese)
+}
+
+func (ese *expressionStringExtractor) VisitOr(e p.Or) {
+	e.Lhs.AcceptVisitor(ese)
+	e.Rhs.AcceptVisitor(ese)
+}
+
+func (ese *expressionStringExtractor) VisitEqual(e p.Equal) {
+	e.Lhs.AcceptVisitor(ese)
+	e.Rhs.AcceptVisitor(ese)
 }
